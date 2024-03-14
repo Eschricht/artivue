e38e8cbd29a3abf6606168dcb45cfc18baeb4648
Johannes Eschricht <johannes@nortic.se>
2024-03-14 14:19:12 +0100

    Update scripts order

--- scripts/build.ts
+++ scripts/build.ts
@@ -5,6 +5,5 @@ import { repoRoot } from './paths'
 execSync('pnpm run clean', { stdio: 'inherit', cwd: repoRoot })
 execSync('pnpm -r --filter=./packages/artivue build', { stdio: 'inherit', cwd: repoRoot })
 execSync('pnpm -r --filter=!./packages/artivue --filter=./packages/* build', { stdio: 'inherit', cwd: repoRoot })
-execSync('pnpm -r --filter=./packages/nuxt prepack', { stdio: 'inherit', cwd: repoRoot })
 
 consola.success('Build complete!')
