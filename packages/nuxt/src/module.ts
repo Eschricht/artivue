4f4b5eee375c0dbfee65ef27be74299428c6b61d
Johannes Eschricht <johannes@nortic.se>
2024-03-14 23:19:14 +0100

    fix: Add nuxt package

---
+++ packages/nuxt/src/module.ts
@@ -0,0 +1,43 @@
+import { addComponent, addImports, addPlugin, createResolver, defineNuxtModule } from '@nuxt/kit'
+import type { Options } from 'artivue'
+
+export type ModuleOptions = Omit<Options, 'registerComponents'>
+
+export default defineNuxtModule<ModuleOptions>({
+  meta: {
+    name: 'artivue',
+    configKey: 'artivue',
+  },
+  defaults: {},
+  setup(moduleOptions, nuxt) {
+    const resolver = createResolver(import.meta.url)
+
+    // Add plugin
+    nuxt.options.runtimeConfig.public.artivue = {
+      ...moduleOptions,
+      registerComponents: false,
+    }
+
+    addImports([{
+      name: 'useThemeLayer',
+      as: 'useThemeLayer',
+      from: 'artivue',
+    }, {
+      name: 'useBaseTheme',
+      as: 'useBaseTheme',
+      from: 'artivue',
+    }])
+
+    addComponent({
+      name: 'ThemeLayer',
+      export: 'ThemeLayer',
+      filePath: 'artivue',
+    })
+
+    addPlugin({
+      src: resolver.resolve('runtime/plugin'),
+      name: 'artivue',
+      mode: 'all',
+    })
+  },
+})
